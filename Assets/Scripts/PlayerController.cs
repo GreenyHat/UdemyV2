aa23c0280c4e76e69a003290b25c1d378ce97a7f
GreenyHat <aramirezgarcia95@gmail.com>
2021-07-06 12:21:57 +0200

    update

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField]
+    float speed;
+
+    void Start()
+    {
+
+        speed = 20;
+    }
+
+    void Update()
+    {
+        //S = s0 + V * t* (dirección)
+        transform.Translate(Vector3.forward * speed * Time.deltaTime);
+    }
+}
